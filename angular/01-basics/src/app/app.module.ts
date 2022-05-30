cbd392bdb8f7b6088874982782635b7d78b93d35
Pekka Tiitinen <pekka-1992@hotmail.com>
2022-05-30 15:54:55 +0200

    modules

--- angular/01-basics/src/app/app.module.ts
+++ angular/01-basics/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { CounterModule } from './counter/counter.module';
+import { LotrModule } from './lotr/lotr.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeroesModule, CounterModule],
+  imports: [BrowserModule, HeroesModule, CounterModule, LotrModule],
   providers: [],
   bootstrap: [AppComponent],
 })
